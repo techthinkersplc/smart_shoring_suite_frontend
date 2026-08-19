f6e0b0ef5ffca29da50a723e051303b031e572ba
techthinkersplc <techthinkers07@gmail.com>
2026-08-19 23:44:08 +0300

    fix: remove .next from git tracking and add to gitignore

--- .next/dev/server/chunks/ssr/_next-internal_server_app_(dashboard)_executive-dashboard_page_actions_11xt4h4.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(dashboard)/executive-dashboard/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28dashboard%29_executive-dashboard_page_actions_11xt4h4.js.map
